bfb49b921d7c7a66c06b8ead0e2bee7dff25fa6a
Kashish Pahuja <147261989+kashishpahuja@users.noreply.github.com>
2025-02-06 00:31:35 +0530

    gtfc

--- my-next-app/src/app/page.js
+++ my-next-app/src/app/page.js
@@ -9,21 +9,11 @@ import Circle from "./components/Circle";
 import Bay from "./components/Bay";
 import Gift from "./components/Gift";
 import ContactPopup from './components/ContactPopup'
-import GiftCards from './components/GiftCards'
 
-import { useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css"; // Import AOS styles
+
 
 export default function Home() {
-    // Initialize AOS when the component mounts
-    useEffect(() => {
-      AOS.init({
-        duration: 1000, // Duration for each animation
-        easing: "ease-in-out", // Easing type
-        once: true,
-      });
-    }, []); // Empty dependency array ensures this only runs once
+
   return (
     <div>
       <main
